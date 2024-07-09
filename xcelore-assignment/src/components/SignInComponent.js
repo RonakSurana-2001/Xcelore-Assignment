c1a71765d9744d5d42a34e3bf1c11d274fd69457
RonakSurana-2001 <ronaksurana2017@gmail.com>
2024-07-09 16:40:51 +0530

    Added Loader

--- xcelore-assignment/src/components/SignInComponent.js
+++ xcelore-assignment/src/components/SignInComponent.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import toast from 'react-hot-toast';
@@ -8,49 +8,61 @@ import { authActions } from "../redux/store";
 
 function SignInComponent() {
 
-    const userEmail=useRef('')
-    const userPassword=useRef('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const dispatch = useDispatch()  
+    const dispatch = useDispatch()
 
-    const handleSubmit=async()=>{
-        try{
-            const user=await axios.post('https://xcelore-assignment.onrender.com/user/userLogin',{
-                email:userEmail.current.value,
-                password:userPassword.current.value
+    const [isLoading, setisLoading] = useState(false)
+
+    console.log(isLoading)
+
+    // console.log(userEmail.current.value,userPassword.current.value)
+
+    const handleSubmit = async () => {
+        setisLoading(true)
+        try {
+            const user = await axios.post('https://xcelore-assignment.onrender.com/user/userLogin', {
+                email,
+                password
             })
-            if(user.data.success){
-                localStorage.setItem("email",userEmail.current.value)
-                localStorage.setItem("token",user.data.jwtToken)
-                localStorage.setItem("isAdmin",user.data.isAdmin)
+            if (user.data.success) {
+                localStorage.setItem("email", email)
+                localStorage.setItem("token", user.data.jwtToken)
+                localStorage.setItem("isAdmin", user.data.isAdmin)
                 toast.success("User Logged Successfully")
                 dispatch(authActions.login())
+                setisLoading(false)
                 navigate("/userdetails")
             }
-            else{
+            else {
+                setisLoading(false)
                 toast.error(user.data.message)
             }
-        } catch(err){
+        } catch (err) {
+            setisLoading(false)
             console.log(err)
         }
     }
 
     return (
         <>
-            <div className='h-screen flex flex-row items-center justify-center'>
+            {!isLoading ? <div className='h-screen flex flex-row items-center justify-center'>
                 <div className='grid grid-row-4 justify-center bg-blue-300 rounded-lg max-md:w-4/5 w-2/6 p-3'>
                     <div className='flex flex-col items-center space-y-6 p-2'>
                         <div className='max-md:text-2xl text-4xl'>Email</div>
-                        <input type="email" placeholder='Email' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' ref={userEmail}/>
+                        <input type="text" placeholder='Email' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className='flex flex-col items-center space-y-6 p-2'>
                         <div className='max-md:text-2xl text-4xl'>Password</div>
-                        <input type="password" placeholder='Password' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' ref={userPassword}/>
+                        <input type="text" placeholder='Password' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className='flex flex-col items-center p-2'>
-                        <button className='text-xl bg-green-600 p-3 rounded-lg' onClick={async()=>{
+                        <button className='text-xl bg-green-600 p-3 rounded-lg' onClick={async () => {
                             handleSubmit()
                         }}>Submit</button>
                     </div>
@@ -58,7 +70,7 @@ function SignInComponent() {
                         <Link to='/signup' className='max-md:text-sm text-3xl'>Not a user register</Link>
                     </div>
                 </div>
-            </div>
+            </div> : <p className='text-center text-red-600 text-6xl'>Loading...</p>}
         </>
     )
 }
--- xcelore-assignment/src/components/SignUpComponent.js
+++ xcelore-assignment/src/components/SignUpComponent.js
@@ -1,55 +1,61 @@
 import axios from 'axios'
-import React, { useRef } from 'react'
+import React, {  useState } from 'react'
 import toast from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 
 function SignUpComponent() {
 
-    const username=useRef('')
-    const email=useRef('')
-    const password=useRef('')
+    const [username,setUsername]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const[loading,setLoading]=useState(false)
 
     const navigate=useNavigate()
 
     const handleSubmit=async()=>{
+        setLoading(true)
         try{
             const user=await axios.post('https://xcelore-assignment.onrender.com/user/userRegister',{
-                email:email.current.value,username:username.current.value,
-                password:password.current.value
+                email,username,
+                password
             })
             if(user.data.success){
                 toast.success(user.data.message)
+                setLoading(false)
                 navigate("/")
             }
             else{
+                setLoading(false)
                 toast.error(user.data.message)
             }
         } catch(err){
+            setLoading(false)
             toast.error("Some Error Occurred try again")
         }
     }
 
     return (
         <>
-            <div className='h-screen flex flex-row items-center justify-center'>
+            {!loading?<div className='h-screen flex flex-row items-center justify-center'>
                 <div className='grid grid-row-4 justify-center bg-blue-300 rounded-lg max-md:w-4/5 w-2/6 p-3'>
                     <div className='flex flex-col items-center space-y-6 p-2'>
                         <div className='max-md:text-2xl text-4xl'>Username</div>
-                        <input type="text" placeholder='Username' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' ref={username}/>
+                        <input type="text" placeholder='Username' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' onChange={(e)=>setUsername(e.target.value)}/>
                     </div>
                     <div className='flex flex-col items-center space-y-6 p-2'>
                         <div className='max-md:text-2xl text-4xl'>Email</div>
-                        <input type="email" placeholder='Email' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' ref={email}/>
+                        <input type="email" placeholder='Email' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' onChange={(e)=>setEmail(e.target.value)}/>
                     </div>
                     <div className='flex flex-col items-center space-y-6 p-2'>
                         <div className='max-md:text-2xl text-4xl'>Password</div>
-                        <input type="password" placeholder='Password' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' ref={password}/>
+                        <input type="password" placeholder='Password' className='max-md:w-4/5 w-80 h-10 rounded-md text-center' onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
                     <div className='flex flex-col items-center p-2'>
                         <button className='text-xl bg-green-600 p-3 rounded-lg' onClick={()=>handleSubmit()}>Submit</button>
                     </div>
                 </div>
-            </div>
+            </div>:<p className='text-center text-red-600 text-6xl'>Loading...</p>}
         </>
     )
 }
